1e95dc5f2791b17ca56595cbbe6d4110235401b9
ahmadjamal <qqsy589@gmail.com>
2022-12-03 14:06:00 +0200

    'sss'

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -19,13 +19,13 @@ export const typeOrmConfig : TypeOrmModuleOptions = {
     ssl: {
         rejectUnauthorized: false,
       },
-    url: 'postgres://vnhaauvcffwinb:4a9df12d227133ed840a05539ab59d5902d0a72e380694f979254c9d412f64e6@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/d9s9trld8fis1c',
+    url: 'postgresql://5941:$jBFQY3QZbBQth47HBnDX@containers-us-west-122.railway.app:5941/railway',
     type: 'postgres',
-    host: 'ec2-54-155-110-181.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    username: 'vnhaauvcffwinb',
-    password: '4a9df12d227133ed840a05539ab59d5902d0a72e380694f979254c9d412f64e6',
-    database: 'd9s9trld8fis1c',
+    host: 'containers-us-west-122.railway.app',
+    port: 5941,
+    username: 'postgres',
+    password: 'jBFQY3QZbBQth47HBnDX',
+    database: 'railway',
     entities: [City, Blog, CityImage, Customer, Membership, Place, Trip, Reservation, ReservationType, Amenity, Review, Interest, Category, Room, MembershipUser],
     synchronize: true
 }
