1b75537633fe98441f31f6afb9086983face46ca
ahmadjamal <qqsy589@gmail.com>
2022-12-04 15:58:39 +0200

    getMempershupUsersByMempershipId

--- src/memberships/memberships.service.ts
+++ src/memberships/memberships.service.ts
@@ -47,6 +47,7 @@ export class MembershipsService {
     async getMembershipById(id: number): Promise<Membership> {
         const found = await this.membershipRepository.createQueryBuilder('Membership')
             .leftJoinAndSelect('Membership.place', 'place')
+            .leftJoinAndSelect('Membership.MembershipUser', 'MembershipUser')
             .where("Membership.id = :id", {id: id})
             .getOne();
 
