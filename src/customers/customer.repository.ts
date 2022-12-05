556f8367a8b1fd3b4d70a29b550126f3a5da7d6d
ahmadjamal <qqsy589@gmail.com>
2022-12-05 17:20:34 +0200

    sendPlaceAndReservationTypeForCustomerReservations10

--- src/customers/customer.repository.ts
+++ src/customers/customer.repository.ts
@@ -98,6 +98,7 @@ export class CustomerRepository extends Repository<Customer> {
         const customer = await this.createQueryBuilder("Customer")
             .leftJoinAndSelect('Customer.reservations', 'reservation')
             .leftJoinAndSelect('reservations.place', 'place')
+
             .getMany();  
         return customer;
        
